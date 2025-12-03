17be2653eadbb51675132d08efef9258ddee96df
diamish <dia.mishra.us@gmail.com>
2025-12-03 09:20:19 -0600

    helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the iostream library for input/output operations
+
+int main() { // The main function, the entry point of the program
+    std::cout << "Hello, World!" << std::endl; // Print "Hello, World!" to the console
+    return 0; // Indicate successful program execution
+}
